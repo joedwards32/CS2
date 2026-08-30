96e7d767ee76aaecf93cea22e29e311d41f91620
Pascal Paulis <ppaulis@gmail.com>
2026-08-30 22:53:57 +0200

    Fixed sed command for CS2_BOT_* env vars
    if there's a single directory in the cfg folder, the sed commands fail. With this fix, sed only targets gamemode* files

--- sniper/etc/entry.sh
+++ sniper/etc/entry.sh
@@ -127,13 +127,13 @@ if [[ ! -z $CS2_LOG_HTTP_URL ]]; then
 fi
 
 if [[ ! -z $CS2_BOT_DIFFICULTY ]] ; then
-    sed -i "s/bot_difficulty.*/bot_difficulty ${CS2_BOT_DIFFICULTY}/" "${STEAMAPPDIR}"/game/csgo/cfg/*
+    sed -i "s/bot_difficulty.*/bot_difficulty ${CS2_BOT_DIFFICULTY}/" "${STEAMAPPDIR}"/game/csgo/cfg/gamemode_*.cfg
 fi
 if [[ ! -z $CS2_BOT_QUOTA ]] ; then
-    sed -ri "s/bot_quota[[:space:]]+.*/bot_quota ${CS2_BOT_QUOTA}/" "${STEAMAPPDIR}"/game/csgo/cfg/*
+    sed -ri "s/bot_quota[[:space:]]+.*/bot_quota ${CS2_BOT_QUOTA}/" "${STEAMAPPDIR}"/game/csgo/cfg/gamemode_*.cfg
 fi
 if [[ ! -z $CS2_BOT_QUOTA_MODE ]] ; then
-    sed -i "s/bot_quota_mode.*/bot_quota_mode ${CS2_BOT_QUOTA_MODE}/" "${STEAMAPPDIR}"/game/csgo/cfg/*
+    sed -i "s/bot_quota_mode.*/bot_quota_mode ${CS2_BOT_QUOTA_MODE}/" "${STEAMAPPDIR}"/game/csgo/cfg/gamemode_*.cfg
 fi
 
 # Rewrite tv_delay in all gamemode_*.cfg files
