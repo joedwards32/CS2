7ca966afffbeb785385f7bd968b4db25bfa79b57
John Edwards <joedwards32@gmail.com>
2026-09-23 19:59:05 +0100

    feat: add support for selecting game version

--- steamrt4/etc/entry.sh
+++ steamrt4/etc/entry.sh
@@ -42,7 +42,7 @@ while [[ $steamcmd_rc != 0 ]] && [[ $attempt -lt $MAX_ATTEMPTS ]]; do
                                 +force_install_dir "${STEAMAPPDIR}" \
                                 +@bClientTryRequestManifestWithoutCode 1 \
 				+login anonymous \
-				+app_update "${STEAMAPPID}" "${VALIDATE}"\
+				+app_update "${STEAMAPPID}" -beta "${STEAMAPPVERSION}" "${VALIDATE}"\
 				+quit
     steamcmd_rc=$?
 done
